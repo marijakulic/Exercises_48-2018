439f05bfc5061b5103aa1d5a5f6af1da8554887f
marijakulic <99marijakulic@gmail.com>
2020-11-05 19:41:39 +0100

    commit3 - model class created

---
+++ Exercise_48-2018/Exercise_48-2018/ExerciseResult.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise_48_2018
+{
+    class ExerciseResult
+    {
+        public ExerciseResult(int id, string studentName, string studentIndeks, int points)
+        {
+            Id = id;
+            StudentName = studentName;
+            StudentIndeks = studentIndeks;
+            Points = points;
+        }
+
+        public int Id { get; set; }
+        public string StudentName { get; set; }
+        public string StudentIndeks { get; set; }
+        public int Points { get; set; }
+    }
+}
